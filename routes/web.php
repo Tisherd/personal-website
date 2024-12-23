86214017351f6d7bc63b4e071f797ed922738497
Tisherd <mtisherd@gmail.com>
2024-12-23 21:01:37 +0800

    correct /login route

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,11 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/login', function () {
-        return redirect('/');
-    });
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 
 
-    Route::get('/', [App\Http\Controllers\AboutController::class, 'index'] )->name('about');
-    Route::get('/work_experience', [App\Http\Controllers\WorkExperienceController::class, 'index'] )->name('work_experience');
-    Route::get('/users', [App\Http\Controllers\UsersController::class, 'index'] )->name('users');
+    Route::get('/', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
+    Route::get('/work_experience', [App\Http\Controllers\WorkExperienceController::class, 'index'])->name('work_experience');
+    Route::get('/users', [App\Http\Controllers\UsersController::class, 'index'])->name('users');
 });
