5eda07f981bf0fd39319c82f691a9ec65a6fc140
Tisherd <mtisherd@gmail.com>
2024-12-23 13:45:33 +0800

    rename project, test commit

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -13,7 +13,7 @@ class AboutController extends Controller
             'full_name' => 'Иванов Андрей Борисович',
             'date_of_birth' => '1997-03-16',
             'photo' => asset('storage/images/avatar.jpg'), // путь к изображению
-            'education' => 'Высшее образование: МГУ, Факультет прикладной математики',
+            'education' => '-',
             'about_me' => 'Я опытный разработчик с более чем 5-летним стажем работы в области веб-разработки.',
             'email' => 'ivan.ivanov@example.com',
             'phone' => '+7 (900) 123-45-67',
