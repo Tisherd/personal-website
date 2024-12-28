ef24b1bf62b86274896e7c25e5f825612a13b471
Tisherd <mtisherd@gmail.com>
2024-12-28 18:26:04 +0800

    create Users/Edit.vue

---
+++ resources/js/Pages/Users/Edit.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { Link, useForm } from '@inertiajs/vue3';
+import MainLayout from "../../Layouts/MainLayout.vue";
+
+const props = defineProps({
+    user: Object,
+});
+
+const form = useForm({
+    login: props.user.login,
+    desc: props.user.desc,
+    role: props.user.role,
+});
+
+function update() {
+    form.put(route('users.update', props.user.id))
+}
+</script>
+
+<template>
+    <MainLayout>
+        <div class="bg-white shadow mb-10 ">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold text-gray-900">
+                    Редактировать пользователя
+                </h1>
+
+                <Link :href="route('users.index')" class="text-indigo-600 hover:text-indigo-900 my-5 block">
+                    Вернуться назад
+                </Link>
+            </div>
+
+            <form @submit.prevent="update">
+                <div class="px-4 py-5 bg-white sm:p-6">
+                    <div class="grid grid-cols-6 gap-6">
+
+                        <div class="col-span-6">
+                            <label class="block text-sm font-medium text-gray-700">Логин</label>
+                            <input :class="{ 'border-red-500': form.errors.login }" v-model="form.login" type="text"
+                                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+
+                            <div class="text-red-500 mt-2" v-if="form.errors.login">{{ form.errors.login }}</div>
+                        </div>
+
+                        <div class="col-span-6">
+                            <label class="block text-sm font-medium text-gray-700">Описание</label>
+                            <textarea
+                                v-model="form.desc"
+                                rows="4"
+                                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                            </textarea>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                    <button type="submit"
+                        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Обновить
+                    </button>
+                </div>
+            </form>
+        </div>
+    </MainLayout>
+</template>
