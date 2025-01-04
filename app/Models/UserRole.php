29a70463f179ef21fcf0744e9860b0dddeb4a559
Tisherd <mtisherd@gmail.com>
2025-01-04 15:24:21 +0800

    add public $timestamp to User and UserRole

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserRole extends Model
 {
+    public $timestamps = true;
+    
     protected $table = 'user_roles';
 
     protected $fillable = ['title'];
