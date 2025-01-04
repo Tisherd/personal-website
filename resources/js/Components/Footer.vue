a9b1debcf806013210db545be5bca96e4f684d82
Tisherd <mtisherd@gmail.com>
2025-01-04 16:45:19 +0800

    upd Footer and GuestLayout

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
     <footer class="bg-gray-600 text-white p-2 pl-20">
-        <p>&copy; {{ new Date().getFullYear() }} My Laravel App. All rights reserved.</p>
+        <p>&copy; {{ new Date().getFullYear() }} Это Footer.</p>
     </footer>
 </template>
 
