e65911693c83d6bb3c4ec66f9afda487194da428
Tisherd <mtisherd@gmail.com>
2025-01-10 17:55:45 +0800

    create ResumeLayout and ResumeLeftPanel

---
+++ resources/js/Layouts/Aside/ResumeLeftPanel.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <aside class="w-64 bg-gray-800 text-white flex flex-col">
+        <div class="p-4 font-bold text-lg border-b border-gray-700">
+            Резюме
+        </div>
+        <nav class="flex-1 p-4 space-y-2">
+            <Link :href="route('resume.about_me.index')" class="block py-2 px-4 rounded hover:bg-gray-700">
+            Обо мне
+            </Link>
+            <Link :href="route('resume.work_experience.index')" class="block py-2 px-4 rounded hover:bg-gray-700">
+            Опыт работы
+            </Link>
+            <!-- <Link :href="route('resume.skills.index')" class="block py-2 px-4 rounded hover:bg-gray-700">
+            Навыки
+            </Link> -->
+            <!-- <Link :href="route('resume.questions.index')" class="block py-2 px-4 rounded hover:bg-gray-700">
+            Вопросы
+            </Link> -->
+        </nav>
+    </aside>
+</template>
---
+++ resources/js/Layouts/ResumeLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HeaderComponent from "@/Components/Header.vue";
+import FooterComponent from "@/Components/Footer.vue";
+import ResumeLeftPanel from "@/Layouts/Aside/ResumeLeftPanel.vue";
+</script>
+
+<template>
+    <div class="bg-gray-100 text-gray-800 flex flex-col min-h-screen">
+        <HeaderComponent />
+        <div class="flex h-screen">
+            <ResumeLeftPanel />
+            <main class="container mx-auto px-4 py-8 flex-grow">
+                <slot />
+            </main>
+        </div>
+        <FooterComponent />
+    </div>
+</template>
+
