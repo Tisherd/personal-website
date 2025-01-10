147f938659928ebea01b1ea93c8375e775cd8148
Tisherd <mtisherd@gmail.com>
2025-01-10 13:49:01 +0800

    turn on middleware admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \App\Http\Middleware\EnsureUserIsAdmin::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class
         ]);
 
         //
