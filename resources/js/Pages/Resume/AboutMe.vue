bda4c2cff9295a404ffef6866c066c9a7e1373ea
Tisherd <mtisherd@gmail.com>
2025-01-14 13:45:31 +0800

    AboutMe centered content

--- resources/js/Pages/Resume/AboutMe.vue
+++ resources/js/Pages/Resume/AboutMe.vue
@@ -11,29 +11,31 @@ defineProps({
 
 <template>
     <ResumeLayout>
-        <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-lg mt-8">
-            <div class="flex items-center mb-4">
-                <img :src="photoUrl" alt="Фото пользователя" class="h-36 rounded-full shadow-lg mr-4" />
-                <div>
-                    <h1 class="text-2xl font-bold">{{ fullName }}</h1>
-                    <p class="text-gray-600"> {{ birthdateFormatted }} ({{ fullAge }} лет)
-                    </p>
+        <div class="flex justify-center bg-gray-100">
+            <div class="bg-white p-6 rounded-lg shadow-md w-full max-w-lg mt-8 ">
+                <div class="flex items-center mb-4">
+                    <img :src="photoUrl" alt="Фото пользователя" class="h-36 rounded-full shadow-lg mr-4" />
+                    <div>
+                        <h1 class="text-2xl font-bold">{{ fullName }}</h1>
+                        <p class="text-gray-600"> {{ birthdateFormatted }} ({{ fullAge }} лет)
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="mt-4">
-                <h2 class="text-xl font-semibold mb-2">Образование</h2>
-                <!-- <p class="text-gray-700">{{ aboutMe.education }}</p> -->
-            </div>
-            <div class="mt-4">
-                <h2 class="text-xl font-semibold mb-2">Обо мне</h2>
-                <!-- <p class="text-gray-700">{{ aboutMe.about_me }}</p> -->
-            </div>
-            <div class="mt-4">
-                <h2 class="text-xl font-semibold mb-2">Контакты</h2>
-                <ul class="list-disc pl-5 text-gray-700">
-                    <!-- <li>Email: {{ aboutMe.email }}</li>
-                    <li>Телефон: {{ aboutMe.phone }}</li> -->
-                </ul>
+                <div class="mt-4">
+                    <h2 class="text-xl font-semibold mb-2">Образование</h2>
+                    <!-- <p class="text-gray-700">{{ aboutMe.education }}</p> -->
+                </div>
+                <div class="mt-4">
+                    <h2 class="text-xl font-semibold mb-2">Обо мне</h2>
+                    <!-- <p class="text-gray-700">{{ aboutMe.about_me }}</p> -->
+                </div>
+                <div class="mt-4">
+                    <h2 class="text-xl font-semibold mb-2">Контакты</h2>
+                    <ul class="list-disc pl-5 text-gray-700">
+                        <!-- <li>Email: {{ aboutMe.email }}</li>
+                        <li>Телефон: {{ aboutMe.phone }}</li> -->
+                    </ul>
+                </div>
             </div>
         </div>
     </ResumeLayout>
