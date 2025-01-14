122346a4d99eea22e4fb2885366e2be0e8c0bf62
Tisherd <mtisherd@gmail.com>
2025-01-14 15:57:39 +0800

    correct Admin/WorkExperienceController

--- app/Http/Controllers/Admin/WorkExperienceController.php
+++ app/Http/Controllers/Admin/WorkExperienceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreWorkExperienceRequest;
 use App\Http\Requests\UpdateWorkExperienceRequest;
-use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 use App\Models\WorkExperience;
 
