1bfe4bc26993f3691bc8b0f5c2e6c73e622d1684
Tisherd <mtisherd@gmail.com>
2025-01-14 12:48:20 +0800

    AboutMeController: add caching

--- app/Http/Controllers/Resume/AboutMeController.php
+++ app/Http/Controllers/Resume/AboutMeController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Resume;
 
 use App\Http\Controllers\Controller;
 use App\Models\AboutMe;
+use Illuminate\Support\Facades\Cache;
 
 class AboutMeController extends Controller
 {
     public function index()
     {
-        $data = AboutMe::firstOrFail();
+        $data = Cache::remember('about_me_data', 60, function () {
+            return AboutMe::firstOrFail();
+        });
 
         return inertia('Resume/AboutMe', [
             'photoUrl' => $data->photo_url,
