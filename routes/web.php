98deae955a6c5dd135daed871eff2ea301b8a42f
Tisherd <mtisherd@gmail.com>
2025-01-15 09:33:13 +0800

    routes/web.php: fix routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::middleware('auth')->group(function () {
         ->name('logout');
 
     Route::get('/', function () {
-        return redirect()->route('resume');
+        return redirect()->route('resume.about_me.index');
     });
 });
 
@@ -27,7 +27,7 @@ Route::prefix('resume')->as('resume.')->middleware('auth')->group(function () {
     Route::get('/about_me', [Controllers\Resume\AboutMeController::class, 'index'])->name('about_me.index');
     Route::get('/work_experience', [Controllers\Resume\WorkExperienceController::class, 'index'])->name('work_experience.index');
     Route::get('/skills', [Controllers\Resume\SkillsController::class, 'index'])->name('skills.index');
-    Route::get('/questions', [Controllers\Resume\WorkExperienceController::class, 'index'])->name('questions.index');
+    Route::get('/questions', [Controllers\Resume\QuestionsController::class, 'index'])->name('questions.index');
 });
 
 
@@ -40,5 +40,5 @@ Route::prefix('admin')->as('admin.')->middleware(['auth', 'admin'])->group(funct
     Route::post('/about_me', [Controllers\Admin\AboutMeController::class, 'update'])->name('about_me.update');
 
     Route::resource('work_experiences', Controllers\Admin\WorkExperienceController::class);
-    Route::resource('users', App\Http\Controllers\Admin\UserController::class);
+    Route::resource('users', Controllers\Admin\UserController::class);
 });
