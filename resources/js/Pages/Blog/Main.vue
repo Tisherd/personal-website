19c484814c38f052b9ee08c55ddf0dd14a1caf40
Tisherd <mtisherd@gmail.com>
2025-01-15 10:51:56 +0800

    create Main.vue for /blog, /projects, /sandbox

---
+++ resources/js/Pages/Blog/Main.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainLayout from "@/Layouts/MainLayout.vue";
+import UnderConstruction from "@/Components/UnderConstruction.vue";
+</script>
+
+<template>
+    <MainLayout>
+        <UnderConstruction />
+    </MainLayout>
+</template>
