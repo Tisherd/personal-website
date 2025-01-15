c3604d2aabb42fba16d66850910918db200dc1f3
Tisherd <mtisherd@gmail.com>
2025-01-15 11:20:51 +0800

    UnderConstruction make mt-32 instead of justify-content

--- resources/js/Components/UnderConstruction.vue
+++ resources/js/Components/UnderConstruction.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-700">
-    <h1 class="text-4xl font-bold mb-4">Страница в разработке</h1>
-    <p class="text-lg mb-6">
-      Мы работаем над созданием этой страницы. Пожалуйста, зайдите позже.
-    </p>
-  </div>
+    <div class="flex flex-col items-center mt-32 bg-gray-100 text-gray-700">
+        <h1 class="text-4xl font-bold mb-4">Страница в разработке</h1>
+        <p class="text-lg mb-6">
+            Мы работаем над созданием этой страницы. Пожалуйста, зайдите позже.
+        </p>
+    </div>
 </template>
