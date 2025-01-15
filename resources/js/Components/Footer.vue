53fabe2037b1a58c23183694dadfb3344e86db2a
Tisherd <mtisherd@gmail.com>
2025-01-15 11:20:13 +0800

    make footer sticky

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -3,7 +3,7 @@
 
 
 <template>
-    <footer class="bg-gray-600 text-white p-2 pl-20">
+    <footer class="fixed bottom-0 left-0 w-full bg-gray-600 text-white p-2 pl-20">
         <p>&copy; {{ new Date().getFullYear() }} Это Footer.</p>
     </footer>
 </template>
