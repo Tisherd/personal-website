cd9c580d6baf1a747cdead8b04284743267cfaf3
Tisherd <mtisherd@gmail.com>
2025-01-16 12:21:16 +0800

    add Projects to admin left panel

--- resources/js/Layouts/Aside/AdminLeftPanel.vue
+++ resources/js/Layouts/Aside/AdminLeftPanel.vue
@@ -4,6 +4,7 @@ import LeftPanel from "@/Components/LeftPanel.vue";
 const links = [
     { href: route('admin.about_me.index'), label: "Обо мне" },
     { href: route('admin.work_experiences.index'), label: "Опыт работы" },
+    { href: route('admin.projects.index'), label: "Проекты" },
     { href: route('admin.users.index'), label: "Пользователи" },
     { href: "/telescope", label: "Telescope" },
 ];
