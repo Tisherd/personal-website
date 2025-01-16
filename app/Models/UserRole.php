49ece25bac24b29184aa57e0b04e088dd0cdc77b
Tisherd <mtisherd@gmail.com>
2025-01-16 14:25:37 +0800

    refactor UserRole model

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -3,16 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class UserRole extends Model
 {
     public $timestamps = true;
-    
+
     protected $table = 'user_roles';
 
     protected $fillable = ['title'];
 
-    public function users()
+    public function users(): HasMany
     {
         return $this->hasMany(User::class, 'role_id');
     }
