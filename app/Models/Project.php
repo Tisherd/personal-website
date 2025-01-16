c473ad06e908024b81bed61b9b190cbc4494eb62
Tisherd <mtisherd@gmail.com>
2025-01-16 12:28:06 +0800

    add some params to Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public $timestamps = true;
+
+    protected $table = 'projects';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'github_url',
+        'live_url',
+    ];
 }
