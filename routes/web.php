70b6c5a5686f7399029f67cdc9b0a524cc42a52b
Tisherd <mtisherd@gmail.com>
2025-01-16 11:28:07 +0800

    refactor routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -23,23 +23,37 @@ Route::middleware('auth')->group(function () {
             return redirect()->route('resume.about_me.index');
         });
 
-        Route::get('/about_me', [Controllers\Resume\AboutMeController::class, 'index'])->name('about_me.index');
-        Route::get('/work_experience', [Controllers\Resume\WorkExperienceController::class, 'index'])->name('work_experience.index');
-        Route::get('/skills', [Controllers\Resume\SkillsController::class, 'index'])->name('skills.index');
-        Route::get('/questions', [Controllers\Resume\QuestionsController::class, 'index'])->name('questions.index');
+        Route::get('/about_me', [Controllers\Resume\AboutMeController::class, 'index'])
+            ->name('about_me.index');
+
+        Route::get('/work_experience', [Controllers\Resume\WorkExperienceController::class, 'index'])
+            ->name('work_experience.index');
+
+        Route::get('/skills', [Controllers\Resume\SkillsController::class, 'index'])
+            ->name('skills.index');
+
+        Route::get('/questions', [Controllers\Resume\QuestionsController::class, 'index'])
+            ->name('questions.index');
     });
 
-    Route::get('/projects', Controllers\Projects\IndexController::class)->name('projects.index');
-    Route::get('/sandbox', Controllers\Sandbox\IndexController::class)->name('sandbox.index');
-    Route::get('/blog', Controllers\Blog\IndexController::class)->name('blog.index');
+    Route::get('/projects', Controllers\Projects\IndexController::class)
+        ->name('projects.index');
+
+    Route::get('/sandbox', Controllers\Sandbox\IndexController::class)
+        ->name('sandbox.index');
+
+    Route::get('/blog', Controllers\Blog\IndexController::class)
+        ->name('blog.index');
 
     Route::prefix('admin')->as('admin.')->middleware('admin')->group(function () {
         Route::get('/', function () {
             return redirect()->route('admin.about_me.index');
         });
 
-        Route::get('/about_me', [Controllers\Admin\AboutMeController::class, 'index'])->name('about_me.index');
-        Route::put('/about_me', [Controllers\Admin\AboutMeController::class, 'update'])->name('about_me.update');
+        Route::get('/about_me', [Controllers\Admin\AboutMeController::class, 'index'])
+            ->name('about_me.index');
+        Route::put('/about_me', [Controllers\Admin\AboutMeController::class, 'update'])
+            ->name('about_me.update');
 
         Route::resource('work_experiences', Controllers\Admin\WorkExperienceController::class)
             ->names('work_experiences');
