72dbac1a777ff57c6567e3378d2fad01705cb67e
Tisherd <mtisherd@gmail.com>
2025-01-16 14:25:54 +0800

    refactor User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -28,15 +30,10 @@ class User extends Authenticatable
 
     protected $appends = ['is_admin'];
 
-    public function role()
+    public function role(): BelongsTo
     {
         return $this->belongsTo(UserRole::class, 'role_id')
             ->withDefault(['title' => 'no_role']);
-    }
-
-    public function getIsAdminAttribute()
-    {
-        return $this->role->title === 'admin';
     }
 
     protected function casts(): array
@@ -44,5 +41,12 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
+    }
+
+    protected function isAdmin(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->role->title === 'admin'
+        );
     }
 }
