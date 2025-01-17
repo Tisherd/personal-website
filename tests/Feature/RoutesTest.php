9b83d52e2cf582a37d59609ac6f3080083d66328
Tisherd <mtisherd@gmail.com>
2025-01-17 14:37:32 +0800

    create test_resume_routes and test_admin_routes

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -29,35 +29,63 @@ class RoutesTest extends TestCase
         $this->adminUser = User::factory()->admin()->create();
     }
 
-    /**
-     * Test guest routes.
-     */
-    public function test_guest_routes(): void
+    public function test_login_route(): void
     {
         $this->get(route('login'))->assertStatus(200);
 
-        $this->get(route('resume.about_me.index'))->assertRedirect(route('login'));
-        $this->get(route('resume.work_experiences.index'))->assertRedirect(route('login'));
-        $this->get(route('resume.skills.index'))->assertRedirect(route('login'));
-        $this->get(route('resume.questions.index'))->assertRedirect(route('login'));
+        $this->actingAs($this->simpleUser);
+
+        $this->get(route('login'))->assertRedirect(route('resume.about_me.index'));
 
         $this->get(route('projects.index'))->assertRedirect(route('login'));
         $this->get(route('sandbox.index'))->assertRedirect(route('login'));
         $this->get(route('blog.index'))->assertRedirect(route('login'));
-
-        $this->get(route('admin.about_me.index'))->assertRedirect(route('login'));
-        $this->get(route('admin.work_experiences.index'))->assertRedirect(route('login'));
-        $this->get(route('admin.projects.index'))->assertRedirect(route('login'));
-        $this->get(route('admin.users.index'))->assertRedirect(route('login'));
-
-        $this->get('/telescope')->assertNotFound();
     }
 
-    public function test_telescope_route(): void
+    public function test_resume_routes(): void
     {
-        $this->get('/telescope')->assertNotFound();
-        $this->actingAs($this->simpleUser)->get('/telescope')->assertNotFound();
-        $this->actingAs($this->adminUser)->get('/telescope')->assertStatus(200);
+        $routes = [
+            route('resume.about_me.index'),
+            route('resume.work_experiences.index'),
+            route('resume.skills.index'),
+            route('resume.questions.index'),
+        ];
+
+        foreach ($routes as $route) {
+            $this->get($route)->assertRedirect(route('login'));
+        }
+
+        $this->actingAs($this->simpleUser);
+
+        foreach ($routes as $route) {
+            $this->get($route)->assertStatus(200);
+        }
+    }
+
+    public function test_admin_routes(): void
+    {
+        $routes = [
+            route('admin.about_me.index'),
+            route('admin.work_experiences.index'),
+            route('admin.projects.index'),
+            route('admin.users.index'),
+        ];
+
+        foreach ($routes as $route) {
+            $this->get($route)->assertNotFound();
+        }
+
+        $this->actingAs($this->simpleUser);
+
+        foreach ($routes as $route) {
+            $this->get($route)->assertNotFound();
+        }
+
+        $this->actingAs($this->adminUser);
+
+        foreach ($routes as $route) {
+            $this->get($route)->assertStatus(200);
+        }
     }
 
     public function test_guest_cannot_access_protected_routes(): void
