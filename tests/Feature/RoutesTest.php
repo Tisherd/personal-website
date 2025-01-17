57d11bb2027669587a126ff3f16433dd42fd0023
Tisherd <mtisherd@gmail.com>
2025-01-17 13:29:37 +0800

    update RoutesTest

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -7,32 +7,56 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 use App\Models\User;
+use Database\Seeders\AboutMeSeeder;
 use Database\Seeders\UserRolesSeeder;
 
 class RoutesTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected User $simpleUser;
+
+    protected User $adminUser;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->seed(UserRolesSeeder::class);
+        $this->seed(AboutMeSeeder::class);
+
+        $this->simpleUser = User::factory()->create();
+        $this->adminUser = User::factory()->admin()->create();
     }
 
     /**
      * Test guest routes.
      */
-    public function test_guest_can_access_login_page(): void
+    public function test_guest_routes(): void
     {
-        $response = $this->get(route('login'));
-        $response->assertStatus(200); // Проверка, что страница доступна
+        $this->get(route('login'))->assertStatus(200);
+
+        $this->get(route('resume.about_me.index'))->assertRedirect(route('login'));
+        $this->get(route('resume.work_experiences.index'))->assertRedirect(route('login'));
+        $this->get(route('resume.skills.index'))->assertRedirect(route('login'));
+        $this->get(route('resume.questions.index'))->assertRedirect(route('login'));
+
+        $this->get(route('projects.index'))->assertRedirect(route('login'));
+        $this->get(route('sandbox.index'))->assertRedirect(route('login'));
+        $this->get(route('blog.index'))->assertRedirect(route('login'));
+
+        $this->get(route('admin.about_me.index'))->assertRedirect(route('login'));
+        $this->get(route('admin.work_experiences.index'))->assertRedirect(route('login'));
+        $this->get(route('admin.projects.index'))->assertRedirect(route('login'));
+        $this->get(route('admin.users.index'))->assertRedirect(route('login'));
+
+        $this->get('/telescope')->assertNotFound();
     }
 
     public function test_guest_cannot_access_protected_routes(): void
     {
         $response = $this->get(route('resume.about_me.index'));
-        $response->assertRedirect(route('login')); // Проверка, что перенаправляется на страницу логина
+        $response->assertRedirect(route('login'));
     }
 
     /**
@@ -40,16 +64,18 @@ class RoutesTest extends TestCase
      */
     public function test_authenticated_user_can_access_resume_routes(): void
     {
-        $user = User::factory()->create(); // Создание тестового пользователя
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
 
-        $this->actingAs($user); // Авторизация пользователя
+        $this->actingAs($user);
 
         $response = $this->get(route('resume.about_me.index'));
-        $response->assertStatus(200); // Проверка доступа к маршруту
+        $response->assertStatus(200);
     }
 
     public function test_authenticated_user_can_access_projects_page(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $this->actingAs($user);
@@ -63,7 +89,7 @@ class RoutesTest extends TestCase
      */
     public function test_admin_can_access_admin_routes(): void
     {
-        $admin = User::factory()->create(['role' => 'admin']); // Создание администратора
+        $admin = User::factory()->admin()->create();
 
         $this->actingAs($admin);
 
@@ -73,12 +99,13 @@ class RoutesTest extends TestCase
 
     public function test_non_admin_cannot_access_admin_routes(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $this->actingAs($user);
 
         $response = $this->get(route('admin.about_me.index'));
-        $response->assertForbidden(); // Проверка, что доступ запрещён
+        $response->assertNotFound();
     }
 
     /**
@@ -86,6 +113,7 @@ class RoutesTest extends TestCase
      */
     public function test_root_redirects_to_resume_about_me(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $this->actingAs($user);
