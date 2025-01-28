e87f4372dae8bac5703a44b87681973ae6e69259
Tisherd <mtisherd@gmail.com>
2025-01-28 10:41:04 +0800

    factory: update: UserFactory del comments & explicit specification of the model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,23 +4,16 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 use App\Models\UserRole;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
+    protected $model = User::class;
+
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
