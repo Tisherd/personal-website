820770b387e671c22131490449756412e94aa75c
Tisherd <mtisherd@gmail.com>
2025-02-03 13:40:12 +0800

    docker: correct mongodb

--- docker/mongodb/init_scripts/init_mongo.js
+++ docker/mongodb/init_scripts/init_mongo.js
@@ -1,33 +1,28 @@
 // Login as administrator (if authorization is enabled)
 const adminDB = db.getSiblingDB('admin');
-adminDB.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
+const isAuthenticated = adminDB.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
 
-const dbName = process.env.MONGO_DB_NAME || 'app_database';
-
-if (!process.env.MONGO_DB_NAME) {
-    print('Warning: MONGO_DB_NAME is not set. Using default name:', dbName);
+if (!isAuthenticated) {
+    print('Error: Authentication failed. Check your username and password.');
+    quit(1);
 }
 
-// Create app database
-const appDB = db.getSiblingDB(dbName);
-appDB.createCollection('example_collection'); // create explicitly
-appDB.example_collection.insertOne({ exampleField: 'exampleValue' });
-print('Created app database:', appDB.getName());
+// Create main database
+createDatabase(process.env.MONGO_DB_NAME);
 
-// Check if APP_ENV is not production
-if (process.env.APP_ENV !== 'production') {
+// Create test database
+if (process.env.APP_ENV && process.env.APP_ENV !== 'production') {
+    createDatabase(process.env.MONGO_TEST_DB_NAME);
+}
 
-    const testDbName = process.env.MONGO_TEST_DB_NAME || 'test_database';
-
-    if (!process.env.MONGO_TEST_DB_NAME) {
-        print('Warning: MONGO_TEST_DB_NAME is not set. Using default name:', testDbName);
+function createDatabase(databaseName) {
+    if (databaseName) {
+        const appDB = db.getSiblingDB(databaseName);
+        appDB.createCollection('example_collection');
+        appDB.example_collection.insertOne({ exampleField: 'exampleValue' });  // create explicitly
+        print('Created database:', appDB.getName());
+    } else {
+        print(`Error: Database name is not set.`);
+        quit(1);
     }
-
-    // Create test database
-    const testDB = db.getSiblingDB(testDbName);
-    testDB.createCollection('example_collection'); // create explicitly
-    testDB.example_collection.insertOne({ exampleField: 'exampleValue' });
-    print('Created test database:', testDB.getName());
-} else {
-    print('APP_ENV is production, skipping test DB creation');
 }
