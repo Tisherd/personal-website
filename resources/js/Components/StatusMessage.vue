1872bc64d5d7180a7495e0e0f32ccca2d8be8a62
Tisherd <mtisherd@gmail.com>
2025-02-10 11:50:29 +0800

    create StatusMessage component

---
+++ resources/js/Components/StatusMessage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+
+const message = computed(() => usePage().props.message);
+
+const isMessageVisible = ref(true);
+
+const hideMessage = () => {
+    isMessageVisible.value = false;
+};
+</script>
+
+<template>
+    <p v-if="message && isMessageVisible"
+        class="bg-indigo-600 h-10 flex items-center justify-center text-sm font-medium text-white px-4 sm:px-6 lg:px-8 cursor-pointer"
+        @click="hideMessage">
+        {{ message }}
+    </p>
+</template>
