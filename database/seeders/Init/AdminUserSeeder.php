b31d68a86bd22664d1f9c543a415f6981a4d7db7
Tisherd <mtisherd@gmail.com>
2025-02-10 12:16:04 +0800

    fix AdminUserSeeder & ThisProjectSeeder

--- database/seeders/Init/AdminUserSeeder.php
+++ database/seeders/Init/AdminUserSeeder.php
@@ -5,15 +5,17 @@ namespace Database\Seeders\Init;
 use Illuminate\Database\Seeder;
 
 use App\Models\User;
+use App\Models\UserRole;
 
 class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->admin()->firstOrCreate([
+        User::firstOrCreate([
             'login' => env('ADMIN_USER_LOGIN'),
             'name' => env('ADMIN_USER_NAME'),
             'password' => env('ADMIN_USER_PASSWORD'),
+            'role_id' => UserRole::where('title', UserRole::ADMIN)->first()->id,
             'desc' => "It's me"
         ]);
     }
--- database/seeders/Init/ThisProjectSeeder.php
+++ database/seeders/Init/ThisProjectSeeder.php
@@ -10,7 +10,7 @@ class ThisProjectSeeder extends Seeder
 {
     public function run(): void
     {
-        Project::factory()->firstOrCreate([
+        Project::firstOrCreate([
             'title' => 'Personal Website',
             'description' => 'A personal website built with Laravel, Inertia.js, and Vue 3 to showcase my resume and projects.',
             'github_url' => "https://github.com/Tisherd/personal-website",
