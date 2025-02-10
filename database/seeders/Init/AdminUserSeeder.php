102649fd0671032adb573382c2ce286692cf26d0
Tisherd <mtisherd@gmail.com>
2025-02-10 12:19:37 +0800

    fix AdminUserSeeder & ThisProjectSeeder

--- database/seeders/Init/AdminUserSeeder.php
+++ database/seeders/Init/AdminUserSeeder.php
@@ -11,12 +11,16 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::firstOrCreate([
-            'login' => env('ADMIN_USER_LOGIN'),
-            'name' => env('ADMIN_USER_NAME'),
-            'password' => env('ADMIN_USER_PASSWORD'),
-            'role_id' => UserRole::where('title', UserRole::ADMIN)->first()->id,
-            'desc' => "It's me"
-        ]);
+        User::firstOrCreate(
+            [
+                'login' => env('ADMIN_USER_LOGIN'),
+            ],
+            [
+                'name' => env('ADMIN_USER_NAME'),
+                'password' => env('ADMIN_USER_PASSWORD'),
+                'role_id' => UserRole::where('title', UserRole::ADMIN)->first()->id,
+                'desc' => "It's me"
+            ]
+        );
     }
 }
--- database/seeders/Init/ThisProjectSeeder.php
+++ database/seeders/Init/ThisProjectSeeder.php
@@ -12,6 +12,7 @@ class ThisProjectSeeder extends Seeder
     {
         Project::firstOrCreate([
             'title' => 'Personal Website',
+        ],[
             'description' => 'A personal website built with Laravel, Inertia.js, and Vue 3 to showcase my resume and projects.',
             'github_url' => "https://github.com/Tisherd/personal-website",
             'live_url' => "https://tisherd.info"
